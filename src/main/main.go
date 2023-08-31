f6de309b31f54e8dd1f3214cfb86b4308cf01a24
stan <stan_wong@foxmail.com>
2023-08-31 17:13:57 +0800

    add tips

--- src/main/main.go
+++ src/main/main.go
@@ -13,22 +13,28 @@ func main() {
 }
 
 func voc() {
-	path := chooseVoc()
-	zh, en := readRaw1(path)
-	switch readAction() {
-	case "1":
-		practice(zh, en)
-	case "2":
-		zhToEn(zh, en)
-	/* case "3":
-	enToZh(zh, en) */
-	default:
-		fmt.Println("unknown action.")
+	for {
+		path := chooseVoc()
+		if path == "" {
+			return
+		}
+		zh, en := readRaw1(path)
+		switch readAction() {
+		case "1":
+			practice(zh, en)
+		case "2":
+			zhToEn(zh, en)
+		case "q":
+			continue
+		default:
+			fmt.Println("unknown action.")
+		}
 	}
 }
 
 func chooseVoc() (path string) {
 	for {
+		fmt.Println("-----choose dictionary-----")
 		fmt.Println("1: 初中-乱序")
 		fmt.Println("2: 高中-乱序")
 		fmt.Println("3: 四级-乱序")
@@ -36,6 +42,9 @@ func chooseVoc() (path string) {
 		fmt.Println("5: 考研-乱序")
 		fmt.Println("6: 托福-乱序")
 		fmt.Println("7: SAT-乱序")
+		fmt.Println("q: quit")
+		fmt.Println("---------------------------")
+		fmt.Print("choose: ")
 		sIn := bufio.NewScanner(os.Stdin)
 		sIn.Scan()
 		choose := sIn.Text()
@@ -61,19 +70,26 @@ func chooseVoc() (path string) {
 		case "7":
 			path = "english-vocabulary/7.SAT-乱序.txt"
 			return
+		case "q":
+			path = ""
+			return
 		}
 	}
 }
 
 func readAction() (action string) {
 	for {
-		fmt.Println("1: practicing")
+		fmt.Println("---------------------------")
+		fmt.Println("1: show vocabulary")
 		fmt.Println("2: zh to en")
+		fmt.Println("q: back to choose dictionary")
+		fmt.Println("---------------------------")
+		fmt.Print("choose: ")
 		// fmt.Println("3: en to zh")
 		sIn := bufio.NewScanner(os.Stdin)
 		sIn.Scan()
 		action = sIn.Text()
-		if "0" == action || "1" == action || "2" == action {
+		if "q" == action || "1" == action || "2" == action {
 			break
 		}
 	}
@@ -88,7 +104,7 @@ func readRaw1(path string) (zh, en []string) {
 	defer f.Close()
 	scaner := bufio.NewScanner(f)
 
-  count := 1
+	count := 1
 	for scaner.Scan() {
 		if err = scaner.Err(); err != nil {
 			panic(err)
@@ -99,9 +115,9 @@ func readRaw1(path string) (zh, en []string) {
 			en = append(en, before)
 			zh = append(zh, strings.Trim(after, " "))
 		}
-    count++
+		count++
 	}
-  fmt.Println(count)
+	fmt.Println("the number of word: ", count)
 	return
 }
 
@@ -164,16 +180,27 @@ func practice(zh, en []string) {
 func zhToEn(zh, en []string) {
 	for {
 		idx := rand.Intn(len(zh))
-		fmt.Println(zh[idx])
 		sIn := bufio.NewScanner(os.Stdin)
+		count := 1
+		fmt.Println("-------------------------------------")
+		fmt.Println(zh[idx])
 		for {
+			fmt.Print("answer: ")
 			sIn.Scan()
 			result := sIn.Text()
 			if result == "j" {
 				fmt.Println(en[idx])
 				break
 			}
+
 			if result == en[idx] {
+				break
+			} else if count <= len(en[idx]) {
+				fmt.Print("tips: ")
+				fmt.Println(en[idx][:count])
+				count++
+			} else if count > len(en[idx]) {
+				fmt.Println("!!!笨蛋!!!")
 				break
 			}
 		}
@@ -192,6 +219,8 @@ func enToZh(zh, en []string) {
 				fmt.Println(zh[idx])
 				break
 			}
+
+			fmt.Println(len(en[idx]))
 			if result == zh[idx] {
 				break
 			}
